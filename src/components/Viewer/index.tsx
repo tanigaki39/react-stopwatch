4109a60f38e3e552f59ab87e33a65e70b4d578c9
tanigaki <junya.tanigaki.39@gmail.com>
2020-11-03 23:43:55 +0900

    chore: Viewコンポーネント内で時間のフォーマット機能を作成

--- src/components/Viewer/index.tsx
+++ src/components/Viewer/index.tsx
@@ -7,9 +7,21 @@ interface CounterProps {
 }
 //____________________________________________
 //
-const Component: React.FC<CounterProps> = ({ timer }) => (
-  <div className={styles.root}>{timer}</div>
-);
+const formatTime = (time: number) => {
+  const milliseconds = `0${time % 100}`.slice(-2);
+  const seconds = `0${Math.floor(time / 100) % 60}`.slice(-2);
+  const minutes = `0${Math.floor(time / 6000) % 60}`.slice(-2);
+  const hours = `${Math.floor(time / 360000)}`;
+  return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+};
+
+const Component: React.FC<CounterProps> = ({ timer }) => {
+  return (
+    <div className={styles.root}>
+      <div>{formatTime(timer)}</div>
+    </div>
+  );
+};
 //____________________________________________
 //
 export default Component;
